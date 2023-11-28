86d45fe408b19cd74748f69de07f040050f5c880
IraitzGarmendia <iraitzete@gmail.com>
2023-11-28 11:54:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #-----------------------------LIBRERIAS-----------------------------
 import streamlit as st
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
